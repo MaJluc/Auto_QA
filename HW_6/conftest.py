b4a22bdf3a942231e98b25cfcf70ebc2e1e8cf85
Maksym_Dodenko <majluc19@gmail.com>
2026-05-27 11:23:45 +0200

    add hw6

---
+++ HW_6/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+
+@pytest.fixture
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(service=Service(),options=chrome_options)
+    # driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    yield driver
+    driver.quit()
